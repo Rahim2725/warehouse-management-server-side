f9f020ce3942db43ae5dd0b7fde723a6f584f827
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-12 13:56:54 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ld5j5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("products").collection("phone");
+  console.log('mongodb connected')
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/', (req, res) => {
     res.send('Ware hous  management running')
 })
