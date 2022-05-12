6b09962accdced8715cd78bdfa0e71e4b7b05b00
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-12 14:23:42 +0600

    get from mongo db server

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000 ;
 const app = express();
-// require("dotenv").config() ;
+require("dotenv").config() ;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 
 //middelware 
@@ -10,16 +11,34 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const user = process.env.DB_USER ;
+const pass = process.env.DB_PASS ;
+
+console.log(user, pass)
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ld5j5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("products").collection("phone");
-  console.log('mongodb connected')
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run() {
+    try{
+        await client.connect();
+        const phoneCollection = client.db('products').collection('phone');
+
+        app.get('/phones', async(req, res) =>{
+            const query = {};
+            const cursor =  phoneCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+
+        })
+
+    }
+    finally{
+        // await client.close
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Ware hous  management running')
