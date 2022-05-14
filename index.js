c22de0d9157178ca0ae365cca7a37ad89c687cae
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-14 07:09:50 +0600

    get phones id

--- index.js
+++ index.js
@@ -3,18 +3,13 @@ const cors = require('cors');
 const port = process.env.PORT || 5000 ;
 const app = express();
 require("dotenv").config() ;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 //middelware 
 app.use(cors());
 app.use(express.json());
 
-
-const user = process.env.DB_USER ;
-const pass = process.env.DB_PASS ;
-
-console.log(user, pass)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ld5j5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -29,8 +24,15 @@ async function run() {
             const cursor =  phoneCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
+        });
 
+        app.get('/phones/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await phoneCollection.findOne(query);
+            res.send(result);
         })
+
 
     }
     finally{
